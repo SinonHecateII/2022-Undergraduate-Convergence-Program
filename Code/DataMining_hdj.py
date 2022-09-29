1a8bc3d0b016485e1220401c38bfed51bd9d7d1e
SinonHecateII <61343001+SinonHecateII@users.noreply.github.com>
2022-09-30 00:55:19 +0900

    Change Crawl Range
    데이터 수집 범위를 2011년~2021년에 한해서만 수집하도록 코드 수정

--- Code/DataMining_hdj.py
+++ Code/DataMining_hdj.py
@@ -14,7 +14,7 @@ head = {'Authorization' : 'token %s' % token}
 
 with conn:
     with conn.cursor() as cur:
-        sql = "SELECT COLUMN_NAME FROM information_schema.COLUMNS WHERE table_schema = 'sys' AND table_name = 'github_db'"  # 테이블의 컬럼명을 가져온다.
+        sql = "SELECT COLUMN_NAME FROM information_schema.COLUMNS WHERE table_schema = 'sys' AND table_name = 'github_db_2011_2021'"  # 테이블의 컬럼명을 가져온다.
         cur.execute(sql)
         result = cur.fetchall()
 
@@ -22,8 +22,8 @@ with conn:
         for i in range(0, len(result)):
             LanguageList.append(result[i][0])
 
-        i = 4035
-        while(i < 40000):
+        i = 0
+        while(i < 50000):
             url = 'https://api.github.com/user/' + str(i)  #id로 유저 이름 찾기
             UrlUser = requests.get(url, headers=head)
             
@@ -47,41 +47,47 @@ with conn:
                     text = UrlDate.text
                     RepositoryDateData = json.loads(text)  #레포지스토리 날짜
                     create_date = RepositoryDateData['created_at']
-                    updated_date = RepositoryDateData['updated_at']
-                    pushed_date = RepositoryDateData['pushed_at']
-
-                    url = 'https://api.github.com/repos/' + str(owner) + '/' + str(RepositoryName) + '/languages' 
-                    UrlLanguage = requests.get(url, headers=head)
-                    text = UrlLanguage.text
-                    LanguageData = json.loads(text)
                     
-                    LanguageKey = list(LanguageData.keys())  #해당 레포지스토리에 있는 언어 종류 추출
-                    
-                    if(LanguageKey == []):  #해당 레포지스토리에 언어가 있는지 확인
-                        print(str(i) + " | " + owner + "의 " +RepositoryName +"에 언어 데이터가 없어 다음 레포지스토리를 탐색합니다.")
-                    else:
-                        sql = "INSERT INTO github_db (User_ID, User_Name, Repository_Name, created_at, updated_at, pushed_at) VALUE(%s, %s,%s,%s,%s, %s)"
-                        cur.execute(sql, (str(i), owner, RepositoryName, str(create_date), str(updated_date), str(pushed_date)))
-                        conn.commit()
+                    year = create_date[0:4]
+                    year = int(year)
+                    if(year >= 2011 and year < 2022):  #2011년부터 2021년까지의 데이터만 저장
+                        updated_date = RepositoryDateData['updated_at']
+                        pushed_date = RepositoryDateData['pushed_at']
 
-                        for k in range(0, len(LanguageKey)):                        
-                            if LanguageKey[k] not in LanguageList:  #해당 이름의 칼럼이 리스트에 존재하지 않는다면
-                                if(str.isalnum(LanguageKey[k]) == False):  #C++과 같은 특수문자인지 검사
-                                    sql = "ALTER TABLE github_db add `" + LanguageKey[k] + "` int NOT NULL default '0'"
-                                else:
-                                    sql = "ALTER TABLE github_db add " + LanguageKey[k] + " int NOT NULL default '0'"
-                                cur.execute(sql)
-                                conn.commit()
-                                LanguageList.append(LanguageKey[k])
-
-                            #해당 언어 Byte 수 삽입    
-                            if(str.isalnum(LanguageKey[k]) == False):
-                                sql = "UPDATE github_db SET `" + LanguageKey[k] + "`=%s WHERE User_Name=%s AND Repository_Name=%s"
-                            else:
-                                sql = "UPDATE github_db SET " + LanguageKey[k] + "=%s WHERE User_Name=%s AND Repository_Name=%s"
-                            cur.execute(sql,(LanguageData[LanguageKey[k]], owner, RepositoryName))
+                        url = 'https://api.github.com/repos/' + str(owner) + '/' + str(RepositoryName) + '/languages' 
+                        UrlLanguage = requests.get(url, headers=head)
+                        text = UrlLanguage.text
+                        LanguageData = json.loads(text)
+                        
+                        LanguageKey = list(LanguageData.keys())  #해당 레포지스토리에 있는 언어 종류 추출
+                        
+                        if(LanguageKey == []):  #해당 레포지스토리에 언어가 있는지 확인
+                            print(str(i) + " | " + owner + "의 " +RepositoryName +"에 언어 데이터가 없어 다음 레포지스토리를 탐색합니다.")
+                        else:
+                            sql = "INSERT INTO github_db_2011_2021 (User_ID, User_Name, Repository_Name, created_at, updated_at, pushed_at) VALUE(%s, %s,%s,%s,%s, %s)"
+                            cur.execute(sql, (str(i), owner, RepositoryName, str(create_date), str(updated_date), str(pushed_date)))
                             conn.commit()
-                        print(str(i) + " | " + owner + "의 " +RepositoryName +"정보를 성공적으로 저장하였습니다.")
+
+                            for k in range(0, len(LanguageKey)):                        
+                                if LanguageKey[k] not in LanguageList:  #해당 이름의 칼럼이 리스트에 존재하지 않는다면
+                                    if(str.isalnum(LanguageKey[k]) == False):  #C++과 같은 특수문자인지 검사
+                                        sql = "ALTER TABLE github_db_2011_2021 add `" + LanguageKey[k] + "` int NOT NULL default '0'"
+                                    else:
+                                        sql = "ALTER TABLE github_db_2011_2021 add " + LanguageKey[k] + " int NOT NULL default '0'"
+                                    cur.execute(sql)
+                                    conn.commit()
+                                    LanguageList.append(LanguageKey[k])
+
+                                #해당 언어 Byte 수 삽입    
+                                if(str.isalnum(LanguageKey[k]) == False):
+                                    sql = "UPDATE github_db_2011_2021 SET `" + LanguageKey[k] + "`=%s WHERE User_Name=%s AND Repository_Name=%s"
+                                else:
+                                    sql = "UPDATE github_db_2011_2021 SET " + LanguageKey[k] + "=%s WHERE User_Name=%s AND Repository_Name=%s"
+                                cur.execute(sql,(LanguageData[LanguageKey[k]], owner, RepositoryName))
+                                conn.commit()
+                            print(str(i) + " | " + owner + "의 " +RepositoryName +"정보를 성공적으로 저장하였습니다.")
+                    else:
+                        print(str(i) + " | " + owner + "의 " +RepositoryName +"가 " + str(year) + "년도여서 다음 레포지스토리를 탐색합니다.")
                 i = i + 1
             except:
                 url = 'https://api.github.com/rate_limit' 
